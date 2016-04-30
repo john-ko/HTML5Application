d97a8d01a57e9d3c7a8ce327ba9f050291672a4d
LinSamuel <samueltanlin@gmail.com>
2016-04-30 14:29:10 -0700

    fixed model

--- app/model.php
+++ app/model.php
@@ -5,7 +5,7 @@ class Model
 	private $servername = 'localhost';
 	private $dbname = 'tnsdb';
 	private $username = 'root';
-	private $password = 'LOLroot1';
+	private $password = '----';
 
 	protected $dbh;
 	protected $stmh;
@@ -25,9 +25,6 @@ class Model
 
 	public function query($sql, array $params)
 	{
-		//$sql =;
-		//$params = [':id' => 1];
-
 		$this->stmh = $this->dbh->prepare($sql);
 		$this->stmh->execute($params);
 
@@ -37,42 +34,6 @@ class Model
 		}
 
 		return $results;
-		//return $this->stmh->fetch(PDO::FETCH_ASSOC);
 	}
 
-	public function queryPics($sql, array $params)
-	{
-		//$sql =;
-		//$params = [':id' => 1];
-
-		$this->stmh = $this->dbh->prepare($sql);
-		$this->stmh->execute($params);
-
-		$results = [];
-		while($row = $this->stmh->fetch(PDO::FETCH_ASSOC)) {
-			$results[] = $row;
-		}
-
-		return $results;
-		//return $this->stmh->fetch(PDO::FETCH_ASSOC);
-	}
-
-	/* potentially use for find		
-		$sql = "select * from ";
-		$sql .= static::$table;
-		$count = 0;
-		foreach($params as $key => $value) {
-			if ($count == 0) {
-				//$sql .= $key . " " . $value;
-				$sql .= " where " . $key . "=" . $value;
-				$count++;
-			}
-			else {
-				$sql .= " and " . $key . "=" . $value;
-			}
-		}
-		$sql .= ";";
-
-		$this->stmh;
-		*/
 }
