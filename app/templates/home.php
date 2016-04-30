ce8efd0bf4f1356ead1ac2b6ed0f9174fdc9d094
Sophia Chan <sophiawc@uci.edu>
2016-04-29 21:45:23 -0700

    templatizing wip

--- app/templates/home.php
+++ app/templates/home.php
@@ -1,1 +1,50 @@
-<h2>this is home.php</h2>
+        </div>
+        
+        <div class="row">
+
+        	<div class="content">
+        		<img class="splash" src="assets/images/pic.jpg" width="700" height="auto" align="middle"/>
+        	</div>
+        	<h2>New Releases for Women</h2>
+        	<div class="third text-center">
+        		<a href="/women/tops/hd-in-paris-laurel-tank"><img src="/assets/images/products/women/tops/laurelTankFront.jpg" alt="productImage" width="150" height="auto"></a>
+        		<p>Laurel Tank</p>
+        		<p>HD in Paris</p>
+        		<p>78.00</p> 
+        	</div>
+        	<div class="third text-center">
+        		<a href="/women/bottoms/cartonnier-charlie-trousers"><img src="/assets/images/products/women/bottoms/charlieTrousersFront.jpg" alt="productImage" width="150" height="auto"></a>
+        		<p>Charlie Trousers</p>
+        		<p>Cartonnier</p>
+        		<p>108.00</p> 
+        	</div>
+        	<div class="third text-center">
+        		<a href="/women/dresses/madame-shoushou-south-island-maxi-dress"><img src="/assets/images/products/women/dresses/southIslandMaxiDressFront.jpg" alt="productImage" width="150" height="auto"></a>
+        		<p>South Island Maxi Dress</p>
+        		<p>Madame Shoushou</p>
+        		<p>248.00</p> 
+        	</div>
+
+        </div>
+
+        <div class = "row">
+        	<h2> New Releases for Men</h2>
+        	<div class="third text-center">
+        		<a href="/men/bottoms/pendleton-cargo-shorts"><img src="/assets/images/products/men/bottoms/cargoShortsFront.jpg" alt="productImage" width="150" height="auto"></a>
+        		<p>Cargo Shorts</p>
+        		<p>Pendleton</p>
+        		<p>79.50</p>
+        	</div>
+        	<div class="third text-center">
+        		<a href="/men/tops/pendleton-long-sleeve-henley"><img src="/assets/images/products/men/tops/longSleeveHenleyFront.jpg" alt="productImage" width="150" height="auto"></a>
+        		<p>Long-sleeve Henley</p>
+        		<p>Pendleton</p>
+        		<p>89.50</p>
+        	</div>
+        	<div class="third text-center">
+        		<a href="/men/tops/pendleton-fitted-tennyson-shirt"><img src="/assets/images/products/men/tops/fittedTennysonShirtFront.jpg" alt="productImage" width="150" height="auto"></a>
+        		<p>Fitted Tennyson Shirt</p>
+        		<p>Pendleton</p>
+        		<p>99.50</p>
+        	</div>
+        </div>
