45d096051328676db426d1e113d94b7438cd6fc2
LinSamuel <samueltanlin@gmail.com>
2016-04-29 18:02:30 -0700

    adding schema structure and added info related to products

---
+++ sql/create_table.sql
@@ -0,0 +1,49 @@
+use tnsdb;
+
+
+create table products(
+id int (11) unsigned NOT NULL AUTO_INCREMENT,
+brand varchar(200) NOT NULL,
+name varchar(80) NOT NULL,
+color varchar (50) NOT NULL,
+price float(6,2),
+default_image varchar(200),
+details varchar(200),
+slug varchar(80) UNIQUE DEFAULT "",
+PRIMARY KEY(id)
+);
+
+create table product_images(
+product_id int (11) unsigned NOT NULL,
+image_id int (11) unsigned NOT NULL
+);
+
+create table images(
+id int(11) unsigned NOT NULL,
+url varchar(200),
+PRIMARY KEY(id)
+);
+
+create table customers(
+id int(11) unsigned NOT NULL,
+first_name varchar(100),
+last_name varchar(100),
+address varchar(100),
+city varchar(100),
+state varchar(100),
+zip  varchar(10)
+);
+
+create table customers_orders(
+customer_id int (11) unsigned NOT NULL,
+order_id int(11) unsigned NOT NULL,
+total float,
+shipping float
+);
+
+create table orders(
+id int(11) unsigned,
+product_id int(11) unsigned ,
+qty int (11) unsigned
+);
+
---
+++ sql/the_product_data.sql
@@ -0,0 +1,163 @@
+INSERT INTO products VALUES(NULL,"Pendleton","The Davis Chinos","Slate", 89.50, "/men/bottoms/theDavisChinosFront.jpg", "Exclusive pattern lines waistband,Cotton,Machine wash,Imported","pendleton-davis-chinos");
+INSERT INTO products VALUES(NULL,"Pendleton","Compass-5 Pocket Pants","Dark Olive", 89.50, "/men/bottoms/compass5PocketPantsFront.jpg","Exclusive pattern lines waistband,cotton,spandex,machine wash, imported","pendleton-compass-5-pocket-pants");
+INSERT INTO products VALUES(NULL,"Pendleton","Cargo Shorts","Stone Grey", 79.50, "/men/bottoms/cargoShortsFront.jpg","10 inches inseam, Cotton/spandex,Machine wash, Imported","pendleton-cargo-shorts");
+INSERT INTO products VALUES(NULL,"Pendleton","Transit Utility Pants","Grey Stone", 89.50, "/men/bottoms/transitUtilityPantsFront.jpg","Exclusive pattern lines waistband,cotton,spandex,machine wash, imported","pendleton-transit-utility-pants");
+INSERT INTO products VALUES(NULL,"Pendleton","Blaine Chambray Shirt","Blue", 139.00, "/men/tops/blaineChambrayShirtFront.jpg","Smooth worsted wool, looks like vintage-washed chambray,Peaked button pockets,Shirttail hem,Dry clean,Imported USA fabric","pendleton-blaine-chambray-shirt");
+INSERT INTO products VALUES(NULL,"Pendleton","Long-sleeve Henley","Light Indigo", 89.50,"/men/tops/longSleeveHenleyFront.jpg","Cotton,Machine wash,Imported","pendleton-long-sleeve-henley");
+INSERT INTO products VALUES(NULL,"Pendleton","Star Wars: A New Hope Tee","Black/Blue", 29.50, "/men/tops/starWarsNewHopeTeeFront.jpg","Exclusive Pendleton design,100% cotton,Machine wash,Made in USA","pendleton-star-wars-a-new-hope-tee");
+INSERT INTO products VALUES(NULL,"Pendleton","Fitted Tennyson Shirt","Indigo Large Check", 99.50, "/men/tops/fittedTennysonShirtFront.jpg","Cotton,Machine wash,Imported,Trim fit,Tapered silhouette,Rounded shirttail hem","pendleton-fitted-tennyson-shirt");
+INSERT INTO products VALUES(NULL,"Pendleton","Short Sands Cardigan","Tan", 149.00, "/men/tops/shortSandsCardiganFront.jpg","Shawl collar,Welt pockets,Cotton,Hand wash,Imported","pendleton-short-sands-cardigan");
+INSERT INTO products VALUES(NULL,"Pendleton","Short-sleeve Polo","Light Indigo", 79.50, "/men/tops/shortSleevePoloFront.jpg","Casual essential,Cotton,Machine wash,Imported","pendleton-short-sleeve-polo");
+
+INSERT INTO products VALUES(NULL,"Level 99","Level 99 Cicerone Shorts","Plum", 68.00,"/women/bottoms/level99CiceroneShortsFront.jpg","Linen,Lyocell,Spandex,Front slash,Back welt pockets,Machine wash,Imported","level-99-cicerone-shorts");
+INSERT INTO products VALUES(NULL,"Cartonnier","Charlie Trousers","Moss", 108.00,"/women/bottoms/charlieTrousersFront.jpg","Cotton,Spandex,Slim tailored fit,Side slant,Back welt pockets,Machine wash,Imported","cartonnier-charlie-trousers");
+INSERT INTO products VALUES(NULL,"McGuire","McGuire Newton Skinny Jeans","Another Dawn", 230.00,"/women/bottoms/mcguireNewtonSkinnyJeansFront.jpg","Cotton,Polyester,Spandex,Skinny fit,Five-pocket styling,Machine wash,Imported","mcguire-newton-skinny-jeans");
+INSERT INTO products VALUES(NULL,"En Elie","Embroidered Sanna Top","Moss", 118.00,"/women/tops/embroideredSannaFront.jpg","Rayon,Cotton,Kimono sleeves,Pullover styling,Dry clean,Made in US","en-elie-embroidered-sanna-top");
+INSERT INTO products VALUES(NULL,"HD in Paris","Laurel Tank","Grey", 78.00,"/women/tops/laurelTankFront.jpg","Linen,Silk,Cropped silhouette,Pullover styling,Machine wash,Imported","hd-in-paris-laurel-tank");
+
+INSERT INTO products VALUES(NULL,"Maeve","Orchid Island Top","Neutral", 78.00,"/women/tops/orchidIslandTopFront.jpg","Cotton,Pleated detail,Pullover styling,Hand wash,Imported","maeve-orchid-island-top");
+INSERT INTO products VALUES(NULL,"Yoana Baraschi","Eyelet Sleeveless Blouse","White", 118.00,"/women/tops/eyeletSleevelessBlouseFront.jpg","Cotton; cotton lining,Eyelet detail,Button front,Dry clean,Imported","yoana-baraschi-eyelet-sleeveless-blouse");
+INSERT INTO products VALUES(NULL,"Madame Shoushou","South Island Maxi Dress","Blue", 248.00,"/women/dresses/southIslandMaxiDressFront.jpg","Cotton,Side zip,Machine wash,Imported","madame-shoushou-south-island-maxi-dress");
+INSERT INTO products VALUES(NULL,"Eva Franco","Chirography Dress","Blue", 188.00,"/women/dresses/chirographyDressFront.jpg","Woven cotton,Polyester lining,Lace cap sleeves,Side pockets,Side zip,Dry clean,Made in USA","eva-franco-chirography-dress");
+INSERT INTO products VALUES(NULL,"Shoushanna","Azura Lace Sheath","Sky", 395.00,"/women/dresses/azuraLaceSheathDressFront.jpg","Nylon-polyester lace,3/4 sleeves,Back zip,Dry clean,Imported","shoushanna-azura-lace-sheath");
+
+
+INSERT INTO images VALUES(1, "/men/bottoms/theDavisChinosFront.jpg"); 
+INSERT INTO images VALUES(2, "/men/bottoms/theDavisChinosBack.jpg");
+INSERT INTO images VALUES(3, "/men/bottoms/compass5PocketPantsFront.jpg");
+INSERT INTO images VALUES(4, "/men/bottoms/compass5PocketPantsBack.jpg");
+INSERT INTO images VALUES(5, "/men/bottoms/compass5PocketPantsMaterial.jpg");
+INSERT INTO images VALUES(6, "/men/bottoms/cargoShortsFront.jpg");
+INSERT INTO images VALUES(7, "/men/bottoms/cargoShortsBack.jpg");
+INSERT INTO images VALUES(8, "/men/bottoms/transitUtilityPantsFront.jpg");
+INSERT INTO images VALUES(9, "/men/bottoms/transitUtilityPantsBack.jpg");
+INSERT INTO images VALUES(10, "/men/bottoms/transitUtilityPantsMaterial.jpg");
+
+INSERT INTO images VALUES(11, "/men/tops/blaineChambrayShirtFront.jpg"); 
+INSERT INTO images VALUES(12, "/men/tops/blaineChambrayShirtBack.jpg");
+INSERT INTO images VALUES(13, "/men/tops/longSleeveHenleyFront.jpg");
+INSERT INTO images VALUES(14, "/men/tops/longSleeveHenleyBack.jpg");
+INSERT INTO images VALUES(15, "/men/tops/starWarsNewHopeTeeFront.jpg");
+INSERT INTO images VALUES(16, "/men/tops/starWarsNewHopeTeeMaterial.jpg");
+INSERT INTO images VALUES(17, "/men/tops/fittedTennysonShirtFront.jpg");
+INSERT INTO images VALUES(18, "/men/tops/fittedTennysonShirtBack.jpg");
+INSERT INTO images VALUES(19, "/men/tops/shortSandsCardiganFront.jpg");
+INSERT INTO images VALUES(20, "/men/tops/shortSandsCardiganBack.jpg");
+
+INSERT INTO images VALUES(21, "/men/tops/shortSleevePoloFront.jpg"); 
+INSERT INTO images VALUES(22, "/men/tops/shortSleevePoloBack.jpg");
+INSERT INTO images VALUES(23, "/women/bottoms/level99CiceroneShortsFront.jpg");
+INSERT INTO images VALUES(24, "/women/bottoms/level99CiceroneShortsBack.jpg");
+INSERT INTO images VALUES(25, "/women/bottoms/level99CiceroneShortsMaterial.jpg");
+INSERT INTO images VALUES(26, "/women/bottoms/charlieTrousersFront.jpg");
+INSERT INTO images VALUES(27, "/women/bottoms/charlieTrousersBack.jpg");
+INSERT INTO images VALUES(28, "/women/bottoms/charlieTrousersMaterial.jpg");
+INSERT INTO images VALUES(29, "/women/bottoms/mcguireNewtonSkinnyJeansFront.jpg");
+INSERT INTO images VALUES(30, "/women/bottoms/mcguireNewtonSkinnyJeansBack.jpg");
+
+INSERT INTO images VALUES(31, "/women/bottoms/mcguireNewtonSkinnyJeansMaterial.jpg"); 
+INSERT INTO images VALUES(32, "/women/tops/embroideredSannaFront.jpg");
+INSERT INTO images VALUES(33, "/women/tops/embroideredSannaBack.jpg");
+INSERT INTO images VALUES(34, "/women/tops/embroideredSannaBackCloseup.jpg");
+INSERT INTO images VALUES(35, "/women/tops/laurelTankFront.jpg");
+INSERT INTO images VALUES(36, "/women/tops/laurelTankBack.jpg");
+INSERT INTO images VALUES(37, "/women/tops/laurelTankMaterial.jpg");
+INSERT INTO images VALUES(38, "/women/tops/orchidIslandTopFront.jpg");
+INSERT INTO images VALUES(39, "/women/tops/orchidIslandTopBack.jpg");
+INSERT INTO images VALUES(40, "/women/tops/orchidIslandTopFrontCloseup.jpg");
+
+INSERT INTO images VALUES(41, "/women/tops/eyeletSleevelessBlouseFront.jpg"); 
+INSERT INTO images VALUES(42, "/women/tops/eyeletSleevelessBlouseBack.jpg");
+INSERT INTO images VALUES(43, "/women/tops/eyeletSleevelessBlouseFrontCloseup.jpg");
+INSERT INTO images VALUES(44, "/women/dresses/southIslandMaxiDressFront.jpg");
+INSERT INTO images VALUES(45, "/women/dresses/southIslandMaxiDressBack.jpg");
+INSERT INTO images VALUES(46, "/women/dresses/southIslandMaxiDressMaterial.jpg");
+INSERT INTO images VALUES(47, "/women/dresses/chirographyDressFront.jpg");
+INSERT INTO images VALUES(48, "/women/dresses/chirographyDressBack.jpg");
+INSERT INTO images VALUES(49, "/women/dresses/chirographyDressMaterial.jpg");
+INSERT INTO images VALUES(50, "/women/dresses/azuraLaceSheathDressFront.jpg");
+
+INSERT INTO images VALUES(51, "/women/dresses/azuraLaceSheathDressBack.jpg");
+INSERT INTO images VALUES(52, "/women/dresses/azuraLaceSheathDressFrontCloseup.jpg");
+
+
+
+
+
+
+
+
+
+INSERT INTO product_images VALUES(1,1);
+INSERT INTO product_images VALUES(1,2);
+INSERT INTO product_images VALUES(2,3);
+INSERT INTO product_images VALUES(2,4);
+INSERT INTO product_images VALUES(2,5);
+INSERT INTO product_images VALUES(3,6);
+INSERT INTO product_images VALUES(3,7);
+INSERT INTO product_images VALUES(4,8);
+INSERT INTO product_images VALUES(4,9);
+INSERT INTO product_images VALUES(4,10);
+
+INSERT INTO product_images VALUES(5,11);
+INSERT INTO product_images VALUES(5,12);
+INSERT INTO product_images VALUES(6,13);
+INSERT INTO product_images VALUES(6,14);
+INSERT INTO product_images VALUES(7,15);
+INSERT INTO product_images VALUES(7,16);
+INSERT INTO product_images VALUES(8,17);
+INSERT INTO product_images VALUES(8,18);
+INSERT INTO product_images VALUES(9,19);
+INSERT INTO product_images VALUES(9,20);
+
+INSERT INTO product_images VALUES(10,21);
+INSERT INTO product_images VALUES(10,22);
+INSERT INTO product_images VALUES(11,23);
+INSERT INTO product_images VALUES(11,24);
+INSERT INTO product_images VALUES(11,25);
+INSERT INTO product_images VALUES(12,26);
+INSERT INTO product_images VALUES(12,27);
+INSERT INTO product_images VALUES(12,28);
+INSERT INTO product_images VALUES(13,29);
+INSERT INTO product_images VALUES(13,30);
+
+INSERT INTO product_images VALUES(13,31);
+INSERT INTO product_images VALUES(14,32);
+INSERT INTO product_images VALUES(14,33);
+INSERT INTO product_images VALUES(14,34);
+INSERT INTO product_images VALUES(15,35);
+INSERT INTO product_images VALUES(15,36);
+INSERT INTO product_images VALUES(15,37);
+INSERT INTO product_images VALUES(16,38);
+INSERT INTO product_images VALUES(16,39);
+INSERT INTO product_images VALUES(16,40);
+
+INSERT INTO product_images VALUES(17,41);
+INSERT INTO product_images VALUES(17,42);
+INSERT INTO product_images VALUES(17,43);
+INSERT INTO product_images VALUES(18,44);
+INSERT INTO product_images VALUES(18,45);
+INSERT INTO product_images VALUES(18,46);
+INSERT INTO product_images VALUES(19,47);
+INSERT INTO product_images VALUES(19,48);
+INSERT INTO product_images VALUES(19,49);
+INSERT INTO product_images VALUES(20,50);
+
+INSERT INTO product_images VALUES(20,51);
+INSERT INTO product_images VALUES(20,52);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
