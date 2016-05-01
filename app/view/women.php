50157bdfadf829fbe2832a4707735ab7963c7667
Sophia Chan <sophiawc@uci.edu>
2016-04-30 18:20:31 -0700

    initial wip

---
+++ app/view/women.php
@@ -0,0 +1,16 @@
+<?php
+        <div class="row">
+
+			<div class="sidebar">
+			
+				<a href="/index.php/women"> <h1 style="color: #5E9DC8">Women</h1> </a>
+				<a href="/index.php/women/tops">
+					<h4>Tops</h4> </a>
+				<a href="/index.php/women/bottoms">
+					<h4>Bottoms</h4> </a>
+				<a href="/index.php/women/dresses">
+					<h4>Dresses</h4> </a>
+			</div>
+            <div id="productList">The product list goes here</div>
+		</div>
+?>
