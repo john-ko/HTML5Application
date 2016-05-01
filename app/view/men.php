2b2dad547248a00cea9138c1ab5af7443a86021e
Sophia Chan <sophiawc@uci.edu>
2016-04-30 18:20:31 -0700

    initial wip

---
+++ app/view/men.php
@@ -0,0 +1,13 @@
+ <?php
+        <div class="row">
+            <div class="sidebar">
+            
+                <a href="/index.php/men"> <h1 style="color: #5E9DC8">Men</h1> </a>
+                <a href="/index.php/men/tops">
+                    <h4>Tops</h4> </a>
+                <a href="/index.php/men/bottoms">
+                    <h4>Bottoms</h4> </a>
+            </div>
+            <div id="productList">The product list goes here</div>
+		</div>
+?>
