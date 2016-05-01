7f2b3258e9c0b2cafeabafdde4bfda6949b631dd
Sophia Chan <sophiawc@uci.edu>
2016-04-30 19:53:01 -0700

    wip

--- app/view/women.php
+++ app/view/women.php
@@ -1,16 +1,36 @@
-<?php
-        <div class="row">
+<pre>
+	<?php var_dump($obj); ?>
 
-			<div class="sidebar">
+	<div class="row">
+
+		<div class="sidebar">
 			
-				<a href="/index.php/women"> <h1 style="color: #5E9DC8">Women</h1> </a>
-				<a href="/index.php/women/tops">
-					<h4>Tops</h4> </a>
+			<a href="/index.php/women"> <h1 style="color: #5E9DC8">Women</h1> </a>
+			<a href="/index.php/women/tops">
+				<h4>Tops</h4> </a>
 				<a href="/index.php/women/bottoms">
 					<h4>Bottoms</h4> </a>
-				<a href="/index.php/women/dresses">
-					<h4>Dresses</h4> </a>
+					<a href="/index.php/women/dresses">
+						<h4>Dresses</h4> </a>
+					</div>
+					<div id="productList">The product list goes here</div>
+					<?php
+
+					foreach($obj as $product) {
+
+						?>
+
+						<img id="bigpic" src="/assets/images/products<?php echo $product->default_image; ?>" alt="productImage" width="auto" height="auto">
+
+						<p><?php echo $product->name; ?></p>
+						<p><?php echo $product->brand; ?></p>
+						<p><?php echo $product->price; ?></p>
+
+
+						<?php
+					}
+					?>
+				</div>
+
 			</div>
-            <div id="productList">The product list goes here</div>
-		</div>
-?>
+
